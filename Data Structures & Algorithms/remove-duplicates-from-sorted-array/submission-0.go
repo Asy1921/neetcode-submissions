43140b2d784f52ebbf48e8679be0d1d3d0138df2
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2026-08-24 22:22:07 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.go
@@ -0,0 +1,14 @@
+
+func removeDuplicates(nums []int) int {
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			if j-i > 1 {
+				nums[i+1] = nums[j]
+			}
+
+			i++
+		}
+	}
+	return i + 1
+}
